a1afa589d96967062d1262621aef5230aac51d54
Charles Schooler <schooler.charles@gmail.com>
2016-11-07 22:38:00 -0500

    Initial Commit

---
+++ app/src/main/java/com/cschooler/dd5ecompanion/CharacterActivity.java
@@ -0,0 +1,14 @@
+package com.cschooler.dd5ecompanion;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class CharacterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character);
+    }
+
+}
---
+++ app/src/main/java/com/cschooler/dd5ecompanion/CharacterDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.cschooler.dd5ecompanion;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class CharacterDetailsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_details);
+    }
+
+}
