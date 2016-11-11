d16f652749aaf3f160bba6e2e4273082029965ae
Charles Schooler <schooler.charles@gmail.com>
2016-11-10 22:32:45 -0500

    Added character data class

---
+++ app/src/main/java/com/thesapphirereserve/dd5ecompanion/Character.java
@@ -0,0 +1,10 @@
+package com.thesapphirereserve.dd5ecompanion;
+
+import java.io.Serializable;
+
+/**
+ * Created by Chaz Schooler on 11/10/2016.
+ */
+
+public class Character implements Serializable {
+}
