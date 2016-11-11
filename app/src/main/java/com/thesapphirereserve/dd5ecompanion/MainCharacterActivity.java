b736c3d228b4cf9960668b9f30201dce623e25c4
Charles Schooler <schooler.charles@gmail.com>
2016-11-10 20:36:53 -0500

    Restart of fresh start

---
+++ app/src/main/java/com/thesapphirereserve/dd5ecompanion/MainCharacterActivity.java
@@ -0,0 +1,13 @@
+package com.thesapphirereserve.dd5ecompanion;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainCharacterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_character_activity);
+    }
+}
